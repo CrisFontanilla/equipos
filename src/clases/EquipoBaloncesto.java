319f8b414580f6e893d9179e4eec6299cec240ab
andresgomez <felipe77639@gmail.com>
2019-04-05 17:35:09 -0500

    tercer comit

--- src/clases/EquipoBaloncesto.java
+++ src/clases/EquipoBaloncesto.java
@@ -1,7 +1,19 @@
 package clases;
 
-public class EquipoBaloncesto {
-
-	
-	
+public class EquipoBaloncesto extends Equipo {
+public static final int MAXIMO = 10;
+/**
+* Adiciona un nuevo jugador al equipo, si no se ha llegado al máximo.
+* @param jugador el nuevo jugador que se adicionará
+* @return boolean indicando si se pudo adicionar o no
+*/
+@Override
+public boolean adicionarJugador(Jugador jugador) {
+if (jugadores.size() < MAXIMO) {
+return super.adicionarJugador(jugador);
 }
+else {
+return false;
+}
+}
+} 
