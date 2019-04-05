410b0f9adb7cd09867ae4e7b93d2369088138ea9
nicolas <nickrija@live.com>
2019-04-05 17:33:03 -0500

    tercer commit

--- src/clases/Equipo.java
+++ src/clases/Equipo.java
@@ -1,7 +1,16 @@
 package clases;
+import java.util.*;
 
 public abstract class Equipo {
-
+	protected List<Jugador> jugadores = new ArrayList<Jugador>();
+	private Jugador entrenador;
 	
+	public Equipo(List<Jugador> jugadores, Jugador entrenador) 
+	{
+		super();
+		this.jugadores=jugadores;
+		this.entrenador=entrenador;
+	}
 	
+	public abstract boolean adicionarJugador(Jugador jugador);
 }
